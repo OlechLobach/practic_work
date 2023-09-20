f0f6405f5367fac1eb1ab842caae8cb4e5e75789
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-09-21 00:28:53 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-import math
-while True:
-    try:
-        diameter = float(input("Введіть діаметер кола:"))
-        break
-    except ValueError as e:
-        print("Введіть правиельний формат діаметру")
+price_per_console = float(input("Введіть вартість однієї ігрової приставки: "))
+quantity = int(input("Введіть кількість ігрових приставок: "))
+discount_percentage = float(input("Введіть відсоток знижки: "))
+choice = input("Оберіть 'загальна сума' або 'вартість однієї приставки' (введіть '1' або '2'): ")
 
-choice = input("оберіть 'площа' або 'перемитер'(ведіть '1' або '2'):")
 if choice == '1':
-    radius = diameter / 2
-    area = math.pi * (radius **2 )
-    print(f"лоща кола з діаметром {diameter} дорівнює {area:.2f} квадратних довжини.")
+    total_price = price_per_console * quantity * (1 - discount_percentage / 100)
+    print(f"рахунок з урахуванням знижки: {total_price:.2f} $.")
 elif choice == '2':
-    circumrefernce = math.pi * diameter
-    print(f"лоща кола з діаметром {diameter} дорівнює {circumrefernce:.2f} одиниць довжини.")
+    discounted_price = price_per_console * (1 - discount_percentage / 100)
+    print(f"Вартість однієї приставки зі знижкою: {discounted_price:.2f} $.")
 else:
-    print("Неправельний вибір. будь ласка, введіть 'площа' або 'периметр'")
+    print("Неправильний вибір. Будь ласка, оберіть 'загальна сума' або 'приставка'.")
